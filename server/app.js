ae4df8762cef47156422d3252234d6ea17532617
Ahmet <ahmetyavuzkanat@outlook.com>
2026-05-02 16:03:01 +0300

    test line for DB

--- server/app.js
+++ server/app.js
@@ -1,6 +1,26 @@
-const  express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const mainRoutes = require('./src/routes/mainRoutes');
+const Order = require('./src/models/Order');
+const sequelize = require('./src/config/db');
+const OrderItem = require('./src/models/OrderItem')
+const Product = require('./src/models/Product');
+const test = require('./test/dbTestCase');
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+// DB Associations
+
+
+// Test 
+test.DB_TEST()
+
+
+// Routes 
+app.use('', mainRoutes);
+
+
+
+
+
 app.listen(port, () => console.log(`http://127.0.0.1:${port}/`))
